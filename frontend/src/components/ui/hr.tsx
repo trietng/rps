422a623e005d0893061a76494c5c1b9a710322ae
trietng <93473943+trietng@users.noreply.github.com>
2024-12-25 23:59:22 +0700

    Complete UI

---
+++ frontend/src/components/ui/hr.tsx
@@ -0,0 +1,5 @@
+export function HR() {
+    return (
+        <hr className="h-px my-4 bg-gray-200 border-0"></hr>
+    )
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,16 @@ import { createRoot } from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import { Dapp } from "@/components/Dapp";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 // This is the entry point of your application, but it just renders the Dapp
 // react component. All of the logic is contained in it.
 createRoot(document.getElementById("root")!).render(
-    <NextUIProvider>
-        <Dapp />
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+        <NextUIProvider>
+            <Dapp />
+        </NextUIProvider>
+    </QueryClientProvider>
 );
